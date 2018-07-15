87f7283c53c3f3e7999d82ecdc1af16ff1a87077
Marissa Zhong <mwz2101@columbia.edu>
2018-07-15 23:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const drivers = ["Milo","Otis","Garfield"];
 function destructivelyAppendDriver(name) {
   drivers.push(name);
 }
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  return drivers.concat(name);
+}
